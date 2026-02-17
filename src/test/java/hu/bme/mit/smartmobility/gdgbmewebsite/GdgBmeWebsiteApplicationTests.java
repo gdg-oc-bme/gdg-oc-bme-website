cdfa6f9412cd56ac15d4ef0ac99e085e0b2d80fc
IslamNizami <islamnizami0046@gmail.com>
2026-02-17 11:36:46 +0400

    Initial commit: Spring Boot project structure

---
+++ src/test/java/hu/bme/mit/smartmobility/gdgbmewebsite/GdgBmeWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bme.mit.smartmobility.gdgbmewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdgBmeWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
